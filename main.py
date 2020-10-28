cb09b86cf6e4714a77e221d675dda29d26a0ef98
Alexi <alexibmost@gmail.com>
2020-10-28 00:31:02 -0700

    color + height

--- main.py
+++ main.py
@@ -24,21 +24,52 @@ def get_time():
     return pygame.time.get_ticks() / 1000
 
 
+def color_lerp(ca, cb, t):
+    r = ca.r * (1-t) + cb.r * t 
+    g = ca.g * (1-t) + cb.g * t 
+    b = ca.b * (1-t) + cb.b * t 
+    return raylibpy.Color(r, g, b, 255)
+
+
+def lerp_color(db):
+    t = db % 10 / 10
+    if db == 0:
+        return raylibpy.Color(raylibpy.BLACK)
+    elif db < 10:
+        return color_lerp(raylibpy.WHITE, raylibpy.BLUE, t)
+    elif db < 20:
+        return color_lerp(raylibpy.BLUE, raylibpy.GREEN, t)
+    elif db < 30:
+        return color_lerp(raylibpy.GREEN, raylibpy.YELLOW, t)
+    elif db < 40:
+        return color_lerp(raylibpy.YELLOW, raylibpy.ORANGE, t)
+    elif db < 50:
+        return color_lerp(raylibpy.ORANGE, raylibpy.RED, t)
+    elif db < 60:
+        return color_lerp(raylibpy.RED, raylibpy.PURPLE, t)
+    else:
+        return raylibpy.Color(raylibpy.PURPLE)
+
+
 def draw_bars(spec, t,  bar_freqs, freqs, tir, fir):
     for i in range(len(bar_freqs)):
         f = bar_freqs[i]
         db = get_decibel(spec, t, tir, f, fir) + 80 
-        x = i * 10
-        y = 400 - db * 2
-        w = 10
-        h = db * 2
+        x = i * 20
+        y = 160 - (int(db / 10) * 20)
+        #y= 160 - db * 2
+        #y=0
+        w = 20
+        h = 160 - y
+        #h=160
+        c = lerp_color(db)
         #print(db, x, y, w, h)
-        raylibpy.draw_rectangle(x, y, w, h, raylibpy.GREEN)
+        raylibpy.draw_rectangle(x, y, w, h, c)
 
 
 def main():
-    song_name = 'Music/Little Dark Age.wav'
-    dur = 45
+    song_name = 'Music/There Might Be Coffee.wav'
+    dur = 240
     # y = audio time series
     # sr = sampling rate of y
     y, sr = librosa.load(song_name, duration=dur)
@@ -49,7 +80,10 @@ def main():
     time_index_ratio = len(times) / times[len(times)-1]
     freq_index_ratio = len(freqs) / freqs[len(freqs)-1]
 
-    bar_freqs = np.arange(100, 8000, 100)
+    f_max = 6000
+    f_min = 100
+    f_step = int((f_max - f_min) / 32)
+    bar_freqs = np.arange(f_min, f_max, f_step)
 
     #display_song(spec, song_name)
 
@@ -57,7 +91,8 @@ def main():
     pymixer.music.load(song_name)
     pymixer.music.play()
 
-    raylibpy.init_window(800, 400, song_name)
+    raylibpy.init_window(640, 160, song_name)
+    raylibpy.set_target_fps(60)
 
     pygame.init()
 
